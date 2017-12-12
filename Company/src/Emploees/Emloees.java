cb78ada9a55ed65a54cd919be5057328d64ddc0b
Владимир <saitpage2011@yandex.ru>
2017-12-12 08:09:12 +0300

    MyBank

--- Company/src/Emploees/Emloees.java
+++ Company/src/Emploees/Emloees.java
@@ -1,5 +1,13 @@
 package Emploees;
 
 public class Emloees {
-    
+    String emploee;
+
+    public String getEmploee() {
+        return emploee;
+    }
+
+    public void setEmploee(String emploee) {
+        this.emploee = emploee;
+    }
 }
